7cde3cfe41e97ce36f347bf67790675262997fc2
Furkan Emin Can <femincanx@gmail.com>
2025-02-10 16:30:11 +0300

    feat: add both parts for day 11 of 2021

---
+++ src/2021/01/p1.js
@@ -0,0 +1,58 @@
+const input = (await Bun.file(`${import.meta.dir}/data.txt`).text())
+  .split('\n')
+  .map((line) => line.split('').map((num) => parseInt(num)));
+
+let flashCount = 0;
+for (let s = 0; s < 100; s++) {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input[i].length; j++) {
+      input[i][j] += 1;
+    }
+  }
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input[i].length; j++) {
+      if (input[i][j] > 9) {
+        flash(i, j);
+      }
+    }
+  }
+}
+
+function flash(row, col) {
+  if (input[row][col] === -1) {
+    return;
+  }
+
+  // Mark octopus to inform that
+  // it already flashed
+  input[row][col] = -1;
+
+  // Adjacents in above and below line
+  for (let j = -1; j <= 1; j += 2) {
+    for (let i = -1; i <= 1; i++) {
+      if (input[row + j]?.[col + i]) {
+        if (input[row + j][col + i] === 9) {
+          flash(row + j, col + i);
+        } else {
+          input[row + j][col + i] += 1;
+        }
+      }
+    }
+  }
+
+  // Adjacents in current line
+  for (let i = -1; i <= 1; i += 2) {
+    if (input[row]?.[col + i]) {
+      if (input[row][col + i] === 9) {
+        flash(row, col + i);
+      } else {
+        input[row][col + i] += 1;
+      }
+    }
+  }
+
+  input[row][col] = 0;
+  flashCount += 1;
+}
+
+console.log(flashCount);
